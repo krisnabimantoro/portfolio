5c88a0b55a5c50ceab5b0aa58dfa5f47a3bd3c6d
krisnabimantoro <krisnabmntr@gmail.com>
2024-01-30 21:20:15 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["montserrat"],
+      },
+      colors:{
+        primary: '#14213d',
+        secondary:'#fcd34d'
+      }
+    },
+    
+  },
+  plugins: [],
+};
