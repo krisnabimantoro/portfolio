5287c07345ec4209084ee9eae58312c06d7e8da3
krisnabimantoro <krisnabmntr@gmail.com>
2024-02-03 13:58:12 +0700

    updated

--- js/modal.js
+++ js/modal.js
@@ -3,7 +3,6 @@ const showModal = document.querySelector(".show-modal");
 const closeModal = document.querySelector(".close-modal")
 const inModal = document.querySelector(".in-modal");
 
-modal.classList.add("hidden")
 showModal.addEventListener("click",function(){
     modal.classList.remove("hidden")
     inModal.setAttribute("data-aos", "zoom-in");
